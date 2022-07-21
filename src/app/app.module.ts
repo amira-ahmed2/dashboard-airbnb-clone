45d35176e96ee24f1ecce5f55092aa26db57349a
amira-ahmed2 <amiraa.amin300@yahoo.com>
2022-07-21 03:06:32 +0200

    side h

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,9 @@ import { NotfoundComponent } from './components/notfound/notfound.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FontAwesomeModule
+
 
   ],
   providers: [],
